f118af61712ea9cdd42a7efbd16907fe54319efa
utkarshnath <nath.utkarsh1@gmail.com>
2018-12-29 17:11:05 +0530

    bst

--- Lecture_19/bst.cpp
+++ Lecture_19/bst.cpp
@@ -33,6 +33,42 @@ void addElement(node ** root,int data){
         }
     }
 }
+int minBst(node * root){
+    if(!root){
+        return -1;
+    }
+    while(root->left){
+        root = root->left;
+    }
+    return root->data;
+}
+bool find(node * root,int data){
+    while(root){
+        if(root->data==data){
+            return true;
+        }
+        if(root->data<data){
+            root = root->right;
+        }else{
+            root = root->left;
+        }
+    }
+    return false;
+}
+void printInRange(node * root,int k1,int k2){
+    if(root==NULL){
+        return;
+    }
+    if(root->data > k1){
+        printInRange(root->left,k1,k2);
+    }
+    if(root->data >=k1 && root->data<=k2){
+        cout<<root->data<<" ";
+    }
+    if(root->data <k2 ){
+         printInRange(root->right,k1,k2);
+    }
+}
 void levelorder(node * root){
     queue<node *>q;
     if(root){
@@ -77,6 +113,92 @@ void inorder(node * root){
     inorder(root->right);
     return;
 }
+node * sortedLL(node * root){
+    if(root==NULL){
+        return NULL;
+    }
+    node * head = sortedLL(root->left);
+    node * it = head;
+    node * temp = new node(root->data);
+    if(head){
+        while(it->left){
+            it = it->left;
+        }
+        it->left = temp;
+    }else{
+        head = temp;
+    }
+    it = temp;
+    temp->left = sortedLL(root->right);
+    return head;
+}
+void printLL(node * root){
+    while(root){
+        cout<<root->data<<" --> ";
+        root = root->left;
+    }
+    cout<<"NULL"<<endl;
+}
+int height(node * root){
+    if(root==NULL){
+        return 0;
+    }
+    return max(height(root->left),height(root->right))+1;
+}
+bool checkbalanced(node * root){
+    if(root==NULL){
+        return true;
+    }
+
+    bool l = checkbalanced(root->left);
+    bool r = checkbalanced(root->right);
+    int lh = height(root->left);
+    int rh = height(root->right);
+    bool x = false;
+    if(abs(lh-rh)<=1){
+        x = true;
+    }
+    return l & r & x;
+}
+pair<bool,int> checkBalFast(node * root){
+    if(root==NULL){
+        pair<bool,int>p;
+        p.first = true;
+        p.second = 0;
+        return p;
+    }
+    pair<bool,int>l =  checkBalFast(root->left);
+    pair<bool,int>r =  checkBalFast(root->right);
+    bool x = false;
+    if(abs(l.second-r.second)<=1){
+        x = true;
+    }
+    pair<bool,int>p;
+    p.first = l.first & r.first & x;
+    p.second = max(l.second,r.second)+1;
+    return p;
+}
+pair<bool,pair<int,int> > checkBst(node* root){
+    if(root==NULL){
+        pair<bool,pair<int,int> > p;
+        p.first = true;
+        p.second.first = INT_MAX;
+        p.second.second = INT_MIN;
+        return p;
+    }
+    pair<bool,pair<int,int> > left = checkBst(root->left);
+    pair<bool,pair<int,int> > right = checkBst(root->right);
+    bool x = false;
+    if(root->data >= left.second.second  && root->data < right.second.first){
+        x = true;
+    }
+    pair<bool,pair<int,int> > p;
+    p.first = left.first & right.first & x;
+    p.second.first = min(left.second.first,root->data);
+    p.second.second = max(right.second.second,root->data);
+    return p;
+}
+
 int main(){
 node * root = NULL;
 addElement(&root,5);
@@ -87,4 +209,6 @@ addElement(&root,10);
 addElement(&root,4);
 addElement(&root,5);
 levelorder(root);
+node * head = sortedLL(root);
+printLL(head);
 }
